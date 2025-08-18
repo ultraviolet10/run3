f0df893b4ff6928acb2472e26f223c028de83918
kshitij-hash <kjangra1557@gmail.com>
2025-08-18 16:05:06 +0530

    fsd:

--- packages/mini-app/next.config.ts
+++ packages/mini-app/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination:
+          "https://api.farcaster.xyz/miniapps/hosted-manifest/0198bcbd-f406-599d-896f-d11d07038dd8",
+        permanent: false, // 307 Temporary Redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
